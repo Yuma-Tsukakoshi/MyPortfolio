6c2ea72cb81c0ab13d33f98401789f57cf8e0ee2
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2025-01-04 00:27:13 +0900

    add:[モックアップ]Worksセクション

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-import { Center } from "@mantine/core";
+import { Box, Center, Stack, Title } from "@mantine/core";
 
 import { HeroContentLeft } from "@/components/hero/HeroContentLeft";
 import { UserInfoIcons } from "@/components/userInfo/UserInfoIcons";
+import { CarouselForWorks } from "@/components/works/CarouselForWorks";
 
 export default function Home() {
   return (
@@ -10,8 +11,31 @@ export default function Home() {
       <HeroContentLeft />
 
       {/* About me セクション*/}
-      <Center m={40}>
-        <UserInfoIcons />
+      <Title order={2} ta="center">
+        About Me
+      </Title>
+      <Center my={40} h={"100vh"}>
+        <Stack>
+          <UserInfoIcons />
+        </Stack>
+      </Center>
+
+      {/* Works セクション */}
+      <Title order={2} ta="center">
+        Works
+      </Title>
+      <Center mt={40} mx={240} h={"100vh"}>
+        <Stack>
+          <Box>
+            <Title order={3}>インターン</Title>
+            <CarouselForWorks />
+          </Box>
+
+          <Box mt={20}>
+            <Title order={3}>個人・チーム開発</Title>
+            <CarouselForWorks />
+          </Box>
+        </Stack>
       </Center>
     </>
   );
