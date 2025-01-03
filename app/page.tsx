5f4340c96a3dc246d96b46e7ae37a5e65dbcaa0b
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2025-01-03 23:22:22 +0900

    add:[モックアップ]about meセクション

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import { HeroContentLeft } from "@/components/hero/HeroContentLeft";
+import { UserInfoIcons } from "@/components/userInfo/UserInfoIcons";
+import { Box, Center } from "@mantine/core";
 
 export default function Home() {
   return (
     <>
+      {/* Hero セクション*/}
       <HeroContentLeft />
+
+      {/* About me セクション*/}
+      <Center m={40}>
+        <UserInfoIcons />
+      </Center>
     </>
   );
 }
