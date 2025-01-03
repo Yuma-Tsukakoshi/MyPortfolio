a38220a9e82efcf556ed294f2e9b8a30c51be1d6
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2025-01-04 00:47:22 +0900

    add:[モックアップ]Researchセクション

---
+++ components/research/ComponentGrid.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Grid, Title } from "@mantine/core";
+
+import { ResearchGridCard } from "@/components/research/ResearchGridCard";
+
+export function ComponentGrid() {
+  return (
+    // Research プロジェクトに合わせて可変にする → 3で割り切れる数にする
+    <Grid>
+      <Grid.Col span={6}>
+        <Title order={4}>SoilSense</Title>
+        <ResearchGridCard />
+      </Grid.Col>
+      <Grid.Col span={6}>
+        <Title order={4}>SoilTiles</Title>
+        <ResearchGridCard />
+      </Grid.Col>
+    </Grid>
+  );
+}
