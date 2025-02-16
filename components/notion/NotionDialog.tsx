edbf0850e24133338b2d9ea2ac00f8ac72ff49c4
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2025-02-16 21:11:53 +0900

    add: エラー対応

--- components/notion/NotionDialog.tsx
+++ components/notion/NotionDialog.tsx
@@ -11,18 +11,36 @@ export default function NotionDialog({
   pageId: string | null;
   onClose: () => void;
 }) {
-  const [pageData, setPageData] = useState<PageObjectResponse>();
+  const [pageData, setPageData] = useState<PageObjectResponse | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     if (pageId) {
       setLoading(true);
+      setError(null);
       fetch(`/api/notion/${pageId}`)
-        .then((res) => res.json())
+        .then((res) => {
+          if (!res.ok) throw new Error("Failed to fetch data");
+          return res.json();
+        })
         .then((data) => setPageData(data))
+        .catch((err) => setError(err.message))
         .finally(() => setLoading(false));
     }
   }, [pageId]);
+
+  const getTitle = () => {
+    if (!pageData) return "No Data";
+    if ("properties" in pageData) {
+      for (const prp of Object.values(pageData.properties)) {
+        if (prp.type === "title" && prp.title.length > 0) {
+          return prp.title[0].plain_text;
+        }
+      }
+    }
+    return "Untitled Page";
+  };
 
   return (
     <Modal
@@ -35,9 +53,11 @@ export default function NotionDialog({
     >
       {loading ? (
         <Loader size="sm" />
+      ) : error ? (
+        <Text>{error}</Text>
       ) : pageData ? (
         <div className="w-full h-[70vh]">
-          <h1>{pageData?.properties?.Name?.title?.[0]?.plain_text}</h1>
+          <h1>{getTitle()}</h1>
         </div>
       ) : (
         <Text>データが取得できませんでした。</Text>
