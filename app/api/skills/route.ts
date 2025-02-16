8835c03727eaed3b5cf5cd3225f9215bb4dc7cf9
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2025-02-16 18:29:17 +0900

    add: skill setの反映完了

---
+++ app/api/skills/route.ts
@@ -0,0 +1,179 @@
+export async function GET() {
+  return Response.json({
+    frontend: [
+      {
+        label: "html",
+        image: "https://skillicons.dev/icons?i=html",
+        is_acquire: true,
+      },
+      {
+        label: "css",
+        image: "https://skillicons.dev/icons?i=css",
+        is_acquire: true,
+      },
+      {
+        label: "javascript",
+        image: "https://skillicons.dev/icons?i=js",
+        is_acquire: true,
+      },
+      {
+        label: "typescript",
+        image: "https://skillicons.dev/icons?i=ts",
+        is_acquire: true,
+      },
+      {
+        label: "react",
+        image: "https://skillicons.dev/icons?i=react",
+        is_acquire: true,
+      },
+      {
+        label: "Material-UI",
+        image: "https://skillicons.dev/icons?i=mui",
+        is_acquire: true,
+      },
+      {
+        label: "tailwindcss",
+        image: "https://skillicons.dev/icons?i=tailwindcss",
+        is_acquire: true,
+      },
+      {
+        label: "nextjs",
+        image: "https://skillicons.dev/icons?i=nextjs",
+        is_acquire: true,
+      },
+      {
+        label: "vuejs",
+        image: "https://skillicons.dev/icons?i=vue",
+        is_acquire: false,
+      },
+      {
+        label: "flutter",
+        image: "https://skillicons.dev/icons?i=flutter",
+        is_acquire: true,
+      },
+      {
+        label: "dart",
+        image: "https://skillicons.dev/icons?i=dart",
+        is_acquire: true,
+      },
+    ],
+    backend: [
+      {
+        label: "php",
+        image: "https://skillicons.dev/icons?i=php",
+        is_acquire: true,
+      },
+      {
+        label: "laravel",
+        image: "https://skillicons.dev/icons?i=laravel",
+        is_acquire: true,
+      },
+      {
+        label: "python",
+        image: "https://skillicons.dev/icons?i=python",
+        is_acquire: true,
+      },
+      {
+        label: "opencv",
+        image: "https://skillicons.dev/icons?i=opencv",
+        is_acquire: true,
+      },
+      {
+        label: "sklearn",
+        image: "https://skillicons.dev/icons?i=sklearn",
+        is_acquire: true,
+      },
+      {
+        label: "pythorch",
+        image: "https://skillicons.dev/icons?i=pytorch",
+        is_acquire: true,
+      },
+      {
+        label: "flask",
+        image: "https://skillicons.dev/icons?i=flask",
+        is_acquire: true,
+      },
+      {
+        label: "django",
+        image: "https://skillicons.dev/icons?i=django",
+        is_acquire: false,
+      },
+      {
+        label: "rails",
+        image: "https://skillicons.dev/icons?i=rails",
+        is_acquire: false,
+      },
+      {
+        label: "ruby",
+        image: "https://skillicons.dev/icons?i=ruby",
+        is_acquire: false,
+      },
+    ],
+    database: [
+      {
+        label: "mysql",
+        image: "https://skillicons.dev/icons?i=mysql",
+        is_acquire: true,
+      },
+      {
+        label: "postgresql",
+        image: "https://skillicons.dev/icons?i=postgres",
+        is_acquire: true,
+      },
+      {
+        label: "mongodb",
+        image: "https://skillicons.dev/icons?i=mongodb",
+        is_acquire: true,
+      },
+    ],
+    infrastructure: [
+      {
+        label: "aws",
+        image: "https://skillicons.dev/icons?i=aws",
+        is_acquire: true,
+      },
+      {
+        label: "vercel",
+        image: "https://skillicons.dev/icons?i=vercel",
+        is_acquire: true,
+      },
+      {
+        label: "heroku",
+        image: "https://skillicons.dev/icons?i=heroku",
+        is_acquire: false,
+      },
+      {
+        label: "firebase",
+        image: "https://skillicons.dev/icons?i=firebase",
+        is_acquire: false,
+      },
+      {
+        label: "azure",
+        image: "https://skillicons.dev/icons?i=azure",
+        is_acquire: false,
+      },
+      {
+        label: "docker",
+        image: "https://skillicons.dev/icons?i=docker",
+        is_acquire: true,
+      },
+    ],
+    tools: [
+      {
+        label: "github",
+        image: "https://skillicons.dev/icons?i=github",
+        is_acquire: true,
+      },
+      {
+        label: "postman",
+        image: "https://skillicons.dev/icons?i=postman",
+        is_acquire: true,
+      },
+      {
+        label: "arduino",
+        image: "https://skillicons.dev/icons?i=arduino",
+        is_acquire: true,
+      },
+    ],
+  });
+}
---
+++ components/skills/UserSkills.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { Card, Container, SimpleGrid, Title } from "@mantine/core";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+export type Skill = {
+  label: string;
+  image: string;
+  is_acquire: boolean;
+};
+
+export type SkillsData = {
+  frontend: Skill[];
+  backend: Skill[];
+  database: Skill[];
+  infrastructure: Skill[];
+  tools: Skill[];
+};
+
+export function GridSkillsSection() {
+  const [skills, setSkills] = useState<SkillsData>({
+    frontend: [],
+    backend: [],
+    database: [],
+    infrastructure: [],
+    tools: [],
+  });
+
+  // API Routesからデータを取得
+  useEffect(() => {
+    const fetchSkills = async () => {
+      try {
+        const response = await fetch("/api/skills");
+        const data: SkillsData = await response.json();
+
+        setSkills({
+          frontend: data.frontend.filter((skill: Skill) => skill.is_acquire),
+          backend: data.backend.filter((skill: Skill) => skill.is_acquire),
+          database: data.database.filter((skill: Skill) => skill.is_acquire),
+          infrastructure: data.infrastructure.filter(
+            (skill: Skill) => skill.is_acquire,
+          ),
+          tools: data.tools.filter((skill: Skill) => skill.is_acquire),
+        });
+      } catch (error) {
+        console.error("Error fetching skills:", error);
+      }
+    };
+    fetchSkills();
+  }, []);
+
+  return (
+    <Container my="md" className="bg-white p-8 pt-2 rounded-2xl shadow-lg">
+      {Object.entries(skills).map(
+        ([category, skillList]) =>
+          (skillList as Skill[]).length > 0 && (
+            <Card key={category} className="mb-6 p-4 rounded-lg shadow-md">
+              <Title order={5} className="mb-4">
+                {category.toUpperCase()}
+              </Title>
+              <SimpleGrid cols={{ base: 2, sm: 3, md: 4, lg: 8 }} spacing="lg">
+                {(skillList as Skill[]).map((skill, index) => (
+                  <div
+                    className="flex justify-center transform transition duration-300 hover:-translate-y-2 cursor-pointer"
+                    key={index}
+                  >
+                    <Image
+                      src={skill.image}
+                      alt={skill.label}
+                      width={100}
+                      height={100}
+                      className="object-cover"
+                    />
+                  </div>
+                ))}
+              </SimpleGrid>
+            </Card>
+          ),
+      )}
+    </Container>
+  );
+}
