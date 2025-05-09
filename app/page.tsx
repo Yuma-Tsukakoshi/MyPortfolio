706eb01fb4f3d8223f814cfb734fe003e39ac7ad
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2025-05-10 08:50:12 +0900

    fix: refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,41 @@
 "use client";
 
-import { Suspense } from "react";
-
 import { AboutMe } from "@/pages/AboutMe";
 import { Career } from "@/pages/Career";
 import { Contact } from "@/pages/Contact";
 import { Footer } from "@/pages/Footer";
 import { Header } from "@/pages/Header";
 import { Hero } from "@/pages/Hero";
-import { Loading } from "@/pages/Loading";
 import { ScrollToTop } from "@/pages/ScrollToTop";
 import { SkillSet } from "@/pages/SkillSet";
 import { Works } from "@/pages/Works";
 
 export default function Home() {
   return (
-    <Suspense fallback={<Loading />}>
-      <div>
-        <Header />
-        <main className="min-h-screen bg-gray-50">
-          <section id="hero">
-            <Hero />
-          </section>
-          <section id="about">
-            <AboutMe />
-          </section>
-          <section id="career">
-            <Career />
-          </section>
-          <section id="skills">
-            <SkillSet />
-          </section>
-          <section id="works">
-            <Works />
-          </section>
-          <section id="contact">
-            <Contact />
-          </section>
-        </main>
-        <Footer />
-        <ScrollToTop />
-      </div>
-    </Suspense>
+    <div>
+      <Header />
+      <main className="min-h-screen bg-gray-50">
+        <section id="hero">
+          <Hero />
+        </section>
+        <section id="about">
+          <AboutMe />
+        </section>
+        <section id="career">
+          <Career />
+        </section>
+        <section id="skills">
+          <SkillSet />
+        </section>
+        <section id="works">
+          <Works />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
+      </main>
+      <Footer />
+      <ScrollToTop />
+    </div>
   );
 }
--- pages/Loading/container.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { Loading as LoadingPresentation } from "./presentation";
-
-export const Loading = () => {
-  return <LoadingPresentation />;
-};
